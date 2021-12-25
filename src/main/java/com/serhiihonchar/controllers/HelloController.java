17052856d7825b6862680376876a7de2f62edf69
Serhii Honchar <sehonc@softserveinc.com>
2021-12-26 01:34:10 +0200

    add controllers

--- src/main/java/com/serhiihonchar/controllers/HelloController.java
+++ src/main/java/com/serhiihonchar/controllers/HelloController.java
@@ -1,11 +1,11 @@
-package com.serhiihonchar;
+package com.serhiihonchar.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hi")
     public String sayHello() {
         return "hello_world";
     }
