a34f6ad3aee4c21a8fa24c470af8198447d5906b
Serhii Honchar <sehonc@softserveinc.com>
2021-12-26 20:42:30 +0200

    add Model

--- src/main/java/com/serhiihonchar/controllers/FirstController.java
+++ src/main/java/com/serhiihonchar/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.serhiihonchar.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/api")
 public class FirstController {
 
-//    @GetMapping("/hello")
+    //    @GetMapping("/hello")
 //    public String helloPage(HttpServletRequest request) {
 //        String name = request.getParameter("name");
 //        String surname = request.getParameter("surname");
@@ -19,9 +20,10 @@ public class FirstController {
 //        return "first/hello";
 //    }
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname + "!");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+//        System.out.println("Hello, " + name + " " + surname + "!");
+        model.addAttribute("message", "Hello, " + name + " " + surname + "!");
         return "first/hello";
     }
 
